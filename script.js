47394a7bb1f63d1f0572519d3f03504a5a34f46c
Ranga A <rangaamsml@gmail.com>
2026-07-25 16:50:54 +0530

    portfolio page

---
+++ script.js
@@ -0,0 +1,67 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 150;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+
+});
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+        topBtn.style.display = "block";
+    }else{
+        topBtn.style.display = "none";
+    }
+
+});
+
+topBtn.onclick = () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+};
+
+new Typed("#typing", {
+
+    strings: [
+        "Artificial Intelligence and Machine Learning Engineer"
+    ],
+
+    typeSpeed: 70,
+
+    backSpeed: 50,
+
+    backDelay: 1500,
+
+    loop: true
+
+});
